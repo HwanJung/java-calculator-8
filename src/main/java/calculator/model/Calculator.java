ff8d43104bdae3011d48f7f0e0a107b1519ebca8
HwanJung <jh013188@gmail.com>
2025-10-17 15:06:58 +0900

    feat: 추출한 숫자 합 반환

---
+++ src/main/java/calculator/model/Calculator.java
@@ -0,0 +1,18 @@
+package calculator.model;
+
+import java.util.List;
+
+public class Calculator {
+    private Parser parser;
+
+    public int calculate(String input) throws IllegalArgumentException{
+        List<Integer> numbers = parser.parseInput(input);
+
+        int sum = 0;
+        for (Integer number : numbers) {
+            sum += number;
+        }
+
+        return sum;
+    }
+}
